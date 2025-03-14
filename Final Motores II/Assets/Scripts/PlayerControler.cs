486cffa72e8bb50f126f82024982a2d23b82bfd0
GustavoAlfonsin <GustavoAriel.Alfonsin@alumnos.uai.edu.ar>
2025-03-13 22:20:45 -0300

    agregando animaciones

--- Final Motores II/Assets/Scripts/PlayerControler.cs
+++ Final Motores II/Assets/Scripts/PlayerControler.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerControler : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Animator animator;
     private float horizontalMovement = 0f;
     [SerializeField] private float movementSpeed;
     [Range(0, 0.3f)][SerializeField] private float motionSmoothing;
@@ -12,9 +13,18 @@ public class PlayerControler : MonoBehaviour
     private Vector3 speed = Vector3.zero;
     private bool lookingToTheRight = true;
 
+    private int hp = 100;
+    private int currentHP;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        currentHP = hp;
+        animator = GetComponent<Animator>();
+        animator.SetBool("IsWalking", true);
+        animator.SetBool("IsShooting", false);
+        animator.SetBool("Damage", false);
+        animator.SetBool("IsDeath", false);
     }
 
     private void Update()
@@ -31,12 +41,22 @@ public class PlayerControler : MonoBehaviour
     {
         Vector3 targetSpeed = new Vector2(movement, rb.velocity.y);
         rb.velocity = Vector3.SmoothDamp(rb.velocity, targetSpeed, ref speed, motionSmoothing);
-        if (movement > 0 && !lookingToTheRight)
+        if (movement != 0)
         {
-            spin();
-        }else if (movement < 0 && lookingToTheRight)
+            animator.SetBool("IsWalking", true);
+            animator.SetBool("IsShooting", false);
+            if (movement > 0 && !lookingToTheRight)
+            {
+                spin();
+            }else if (movement < 0 && lookingToTheRight)
+            {
+                spin();
+            }
+        }
+        else
         {
-            spin();
+            animator.SetBool("IsWalking", false);
+            animator.SetBool("IsShooting", false);
         }
     }
 
@@ -44,5 +64,19 @@ public class PlayerControler : MonoBehaviour
     {
         lookingToTheRight = !lookingToTheRight;
         transform.eulerAngles = new Vector3(0, transform.eulerAngles.y + 180, 0);
+    }
+
+    public void getDamage(int injury)
+    {
+        currentHP -= injury;
+        if (currentHP <= 0)
+        {
+            death();
+        }
+    }
+
+    private void death()
+    {
+        // hace la animación de muerte y muestra el menu de reinicio de nivel
     }
 }
