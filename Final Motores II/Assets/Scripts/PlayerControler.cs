bc5f51b9c66ae4785e7ca7a34a7d1ddc4f4bb74b
GustavoAlfonsin <GustavoAriel.Alfonsin@alumnos.uai.edu.ar>
2025-03-14 21:39:27 -0300

    mejorando las animaciones

--- Final Motores II/Assets/Scripts/PlayerControler.cs
+++ Final Motores II/Assets/Scripts/PlayerControler.cs
@@ -13,13 +13,27 @@ public class PlayerControler : MonoBehaviour
     private Vector3 speed = Vector3.zero;
     private bool lookingToTheRight = true;
 
+    //Disparo
+    [SerializeField] private Transform gunController;
+    [SerializeField] private float range;
+    private bool isShooting;
+    private float waitTime = 0.517f;
+
+    // Vida jugador
     private int hp = 100;
     private int currentHP;
+    private bool isTakingDamage;
+    [SerializeField] private bool isDeath;
+    private float damagaTime = 0.350f;
+    private float deathTime = 1.017f;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         currentHP = hp;
+        isShooting = false;
+        isTakingDamage = false;
+        isDeath = false;
         animator = GetComponent<Animator>();
         animator.SetBool("IsWalking", true);
         animator.SetBool("IsShooting", false);
@@ -29,14 +43,33 @@ public class PlayerControler : MonoBehaviour
 
     private void Update()
     {
+        if (Input.GetButtonDown("Jump") && isShooting == false && isDeath == false)
+        {
+            isShooting = true;
+            rb.velocity = Vector2.zero;
+            Debug.Log("MUERE, MUERE, MUERE");
+            animator.SetBool("IsShooting", true);
+            StartCoroutine("waitShot");
+            fire();
+        }
+        //BORRAR LO SIGUIENTE
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            getDamage(20);
+            Debug.Log($"{currentHP}");
+        }
         horizontalMovement = Input.GetAxisRaw("Horizontal") * movementSpeed;
     }
 
     private void FixedUpdate()
     {
-        Move(horizontalMovement * Time.fixedDeltaTime);
+        if (isShooting == false && isTakingDamage == false && isDeath == false)
+        {
+            Move(horizontalMovement * Time.fixedDeltaTime);
+        }
     }
 
+    //################## MOVIMIENTO DEL PERSONAJE #######################
     private void Move(float movement)
     {
         Vector3 targetSpeed = new Vector2(movement, rb.velocity.y);
@@ -44,7 +77,6 @@ public class PlayerControler : MonoBehaviour
         if (movement != 0)
         {
             animator.SetBool("IsWalking", true);
-            animator.SetBool("IsShooting", false);
             if (movement > 0 && !lookingToTheRight)
             {
                 spin();
@@ -55,8 +87,8 @@ public class PlayerControler : MonoBehaviour
         }
         else
         {
+            rb.velocity = Vector3.zero;
             animator.SetBool("IsWalking", false);
-            animator.SetBool("IsShooting", false);
         }
     }
 
@@ -66,17 +98,64 @@ public class PlayerControler : MonoBehaviour
         transform.eulerAngles = new Vector3(0, transform.eulerAngles.y + 180, 0);
     }
 
+    //#################### CONTROL DE LA VIDA DEL PERSONAJE ###############################
     public void getDamage(int injury)
     {
         currentHP -= injury;
+
         if (currentHP <= 0)
         {
+            Debug.Log("Esta muerta");
             death();
+            return;
         }
+
+        rb.velocity = Vector2.zero;
+        isTakingDamage = true;
+        animator.SetBool("Damage", true);
+        StartCoroutine("waitDamage");
     }
 
     private void death()
     {
-        // hace la animación de muerte y muestra el menu de reinicio de nivel
+        isDeath = true;
+        isTakingDamage = false;
+        animator.SetBool("Damage", false);
+        animator.SetBool("IsWalking", false);
+        animator.SetBool("IsShooting", false);
+        animator.SetBool("IsDeath", true);
+        StartCoroutine("showDeath");
+    }
+
+    // ########################### ATAQUE DEL PERSONAJE #####################################
+    private void fire()
+    {
+        RaycastHit2D ray = Physics2D.Raycast(gunController.position, gunController.right, range);
+        if (ray)
+        {
+            if (ray.transform.CompareTag("Zombie"))
+            {
+                //Hacer que la barra de vida del zombie baje
+            }
+        }
+    }
+
+    IEnumerator waitShot()
+    {
+        yield return new WaitForSeconds(waitTime);
+        animator.SetBool("IsShooting", false);
+        isShooting = false;
+    }
+
+    IEnumerator waitDamage()
+    {
+        yield return new WaitForSeconds(damagaTime);
+        isTakingDamage = false;
+        animator.SetBool("Damage", false);
+    }
+
+    IEnumerator showDeath()
+    {
+        yield return new WaitForSeconds(deathTime);
     }
 }
